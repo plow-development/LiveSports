e8ca242aaacba0feb6b05150b3e3f8477761a0fb
RUAZD <96168542+RUAZD@users.noreply.github.com>
2022-07-14 18:27:14 +0300

    Главный файл приложения

---
+++ app/api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
+from app.exceptions import CommonException
+from app.services.database import DataBase
+
+app = FastAPI(title='«Умный город: Живи спортом»')
+static = StaticFiles(directory='resources/avatars')
+
+@app.on_event('startup')
+async def startup() -> None:
+    await DataBase.connect_db()
+
+@app.on_event('shutdown')
+async def shutdown() -> None:
+    await DataBase.disconnect_db()
+
+@app.exception_handler(CommonException)
+async def handler_badrequest(requests: Request, exception: CommonException) -> JSONResponse:
+    return JSONResponse(status_code=exception.code, content={'details': exception.message})
+
+app.mount('/resources/avatars', static)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],  # http/frontend # *
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
