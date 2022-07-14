c303023aff2ef31f354a499bfd38ce136c9812ea
RUAZD <96168542+RUAZD@users.noreply.github.com>
2022-07-14 19:39:35 +0300

    Деплоу

--- app/migrations/initial.sql
+++ app/migrations/initial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users
 (
     id              serial PRIMARY KEY,
-    username        text      not null,
+    username        text      not null unique,
     hashed_password text      not null,
     email           text      not null,
     avatar          text      not null,
