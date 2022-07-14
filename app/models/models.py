77eb241f7771b9500b279e36c25b51ec02962d27
RUAZD <96168542+RUAZD@users.noreply.github.com>
2022-07-15 00:33:16 +0300

    Обновление моделей

--- app/models/models.py
+++ app/models/models.py
@@ -3,69 +3,39 @@ from datetime import date
 from pydantic import BaseModel, Field, EmailStr
 
 
-# UserOut
-
-class BaseUserOut(BaseModel):
-    username: str = Field(..., description='Ник пользователя')
-    email: EmailStr = Field(..., description='Почта пользователя')
-    avatar_url: str = Field(..., description='Аватарка пользователя')
-    firstname: str = Field(..., description='Имя пользователя')
-    lastname: str = Field(..., description='Фамилия пользователя')
-    birthday: date = Field(..., description='День рожденья пользователя')
-    money: int = Field(..., description='Баллы пользователя')
-
-
-class UsersTeamOut(BaseModel):
-    name: str = Field(None, description='Команда участников')
+class UserOut(BaseModel):
+    user_id: int = Field(None, description='ID пользователя')
+    username: str = Field(None, description='Ник пользователя')
+    email: EmailStr = Field(None, description='Почта пользователя')
+    avatar: str = Field(None, description='Аватарка пользователя')
+    firstname: str = Field(None, description='Имя пользователя')
+    lastname: str = Field(None, description='Фамилия пользователя')
+    birthday: date = Field(None, description='День рожденья пользователя')
+    money: int = Field(None, description='Баллы пользователя')
 
 
-class BaseSportOut(BaseModel):
-    name: str = Field(None, description='Вид спорта')
+class SportOut(BaseModel):
+    sport_id: int = Field(None, description='ID вида спорта')
+    name: str = Field(None, description='Название вида спорта')
+    description: str = Field(None, description='Описание вида спорта')
+    sport_type: str = Field(None, description='Тип вида спорта')
+
+
+class TeamOut(BaseModel):
+    team_id: int = Field(None, description='ID команды')
+    name: str = Field(None, description='Название команды')
+    master_id: int = Field(None, description='ID пользователя - владельца команды')
+    sport_id: int = Field(None, description='ID вида спорта команды')
 
 
 class UserComplex(BaseModel):
-    user: BaseUserOut = Field(...)
-    teams: list[UsersTeamOut] = Field(None)
-    sport_types: list[BaseSportOut] = Field(None)
+    user: UserOut = Field(None, description='Участник')
+    teams: list[TeamOut] = Field(None, description='Команды, в которых состоит участник')
+    sports: list[SportOut] = Field(None, description='Предпочитаемые виды спорта')
 
 
-# UserOutInList
-
-class BaseUserOutInList(BaseModel):
-    username: str = Field(..., description='Ник пользователя')
-    email: EmailStr = Field(..., description='Почта пользователя')
-    avatar_url: str = Field(..., description='Аватарка пользователя')
-    firstname: str = Field(..., description='Имя пользователя')
-    lastname: str = Field(..., description='Фамилия пользователя')
-    birthday: date = Field(..., description='День рожденья пользователя')
-    money: int = Field(..., description='Баллы пользователя')
-
-
-class UserComplexInList(BaseModel):
-    user: BaseUserOutInList = Field(...)
-    team: list[UsersTeamOut] = Field(None)
-    sport_type: list[BaseSportOut] = Field(None)
-
-
-# Sports
-
-class SportsOut(BaseModel):
-    name: str = Field(..., description='Название вида спорта')
-    description: str = Field(..., description='Описание вида спорта')
-    sport_type: str = Field(..., description='Тип вида спорта')
-
-# Teams
-
-class TeamNameOut(BaseModel):
-    name: str = Field(None, description='Название команды')
-
-class TeamUserNameOut(BaseModel):
-    username: str = Field(None, description='Псевдоним владельца команды')
-
-class TeamSportNameOut(BaseModel):
-    name: str = Field(None, description='Вид спорта команды')
-
-class TeamComplexOut(BaseModel):
-    team_name: TeamNameOut = Field(None, description='Название команды')
-    master_name: TeamUserNameOut = Field(None, description='Псевдоним владельца команды')
-    sport_name: TeamSportNameOut = Field(None, description='Вид спорта команды')
+class TeamComplex(BaseModel):
+    team: TeamOut = Field(None, description='Название команды')
+    master_id: UserOut = Field(None, description='Владелец команды')
+    sport: SportOut = Field(None, description='Вид спорта команды')
+    users: UserOut = Field(None, description='Участники команды')
