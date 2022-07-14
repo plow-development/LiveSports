930dc6f3795c7006adc050a8264889fd4ce518e8
RUAZD <96168542+RUAZD@users.noreply.github.com>
2022-07-14 19:25:20 +0300

    Обработка базы данных пользователей

---
+++ app/queries/users.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+
+import asyncpg
+import hashlib
+
+from fastapi import UploadFile
+from app.exceptions import BadRequest, NotFound
+from app.services.database import DataBase
+
+"""
+CREATE TABLE users
+(
+    id              serial PRIMARY KEY,
+    username        text      not null unique,
+    hashed_password text      not null,
+    email           text      not null unique,
+    avatar          text      not null,
+    firstname       text      not null,
+    lastname        text      not null,
+    birthday        timestamp not null,
+    type            text      not null,
+    money           integer   not null
+);
+"""
+
+async def add_user(username: str, hashed_password: str, email: str, image: UploadFile,
+                   firstname: str, lastname: str, birthday: datetime, type_: str, money: int) -> None:
+    """
+    Создаёт пользователя в базе данных
+
+    :param username: Псевдоним пользователя
+    :param hashed_password: Хэшированный пароль
+    :param email: Электронная почта пользователя
+    :param image: Ссылка на аватар пользователя
+    :param firstname: Имя пользователя
+    :param lastname: Фамилия пользователя
+    :param birthday: День Рожденья пользователя
+    :param type_: Тип пользователя (TODO)
+    :param money: Баланс пользователя (в баллах)
+    """
+    sql = """INSERT INTO users(username, hashed_password, email, avatar, firstname, lastname, birthday, type, money)
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+             """
+    if image is not None:
+        image_data = image.file.read()
+        image_extension = image.filename.split('.')[-1]
+        image_name = f'resources/avatars/{hashlib.sha224(image_data).hexdigest()}.{image_extension}'
+        with open(image_name, mode='wb+') as image_file:
+            image_file.write(image_data)
+    else:
+        image_name = None
+    try:
+        await DataBase.execute(
+            sql, username, hashed_password, email, image_name, firstname, lastname, birthday, type_, money)
+    except asyncpg.UniqueViolationError as e:
+        raise BadRequest("Пользователь уже существует!") from e
+
+async def get_user(user_id: int) -> asyncpg.Record:
+    sql = """SELECT username, hashed_password, email, avatar, firstname, lastname, birthday, type, money FROM users
+             WHERE id = $1
+             """
+    result = await DataBase.fetch(sql, user_id)
+    if not result:
+        raise NotFound("Пользователь не существует!")
+    return result[0]
+
+async def get_user_team(user_id: int) -> list[asyncpg.Record]:
+    """
+    Возвращает из базы данных список команд, в которых состоит пользователь
+
+    :param user_id: ID пользователя
+    :return:
+    """
+    sql = """SELECT teams.name FROM teams_users
+             left   join teams
+             on     teams_users.team_id = teams.id
+             WHERE  teams_users.user_id = $1
+             """
+    result = await DataBase.fetch(sql, user_id)
+    return result
+
+async def get_user_sports(user_id: int) -> list[asyncpg.Record]:
+    """
+    Возвращает список предпочитаемых видов спорта пользователя из базы данных
+
+    :param user_id: ID пользователя
+    """
+    sql = """SELECT sports.name FROM user_sports
+             left   join sports
+             on     user_sports.sport_id = sports.id
+             WHERE  user_sports.user_id = $1
+             """
+    result = await DataBase.fetch(sql, user_id)
+    return result
+
+async def get_list_users() -> list[asyncpg.Record]:
+    """
+    Возвращает список пользователей из базы данных
+    """
+    sql = """SELECT id, username, email, avatar, firstname, lastname, birthday, type, money FROM users"""
+    result = await DataBase.fetch(sql)
+    return result
+
+async def user_sport_add(user_id: int, sport_id: int) -> None:
+    """
+    Добавляет пользователю любимый вид спорта в базе данных
+
+    :param user_id: ID пользователя
+    :param sport_id: ID вида спорта
+    :return:
+    """
+    sql = """INSERT INTO user_sports(user_id, sport_id) VALUES ($1, $2)"""
+    try:
+        await DataBase.execute(sql, user_id, sport_id)
+    except asyncpg.UniqueViolationError as e:
+        raise BadRequest(f'Вы уже выбирали этот вид спорта!') from e
+
+async def del_user(user_id: int) -> None:
+    """
+    Удаляет пользователя из базы данных
+
+    :param user_id: ID пользователя
+    """
+    sql = """DELETE FROM users WHERE id = ($1)"""
+    await DataBase.execute(sql, user_id)
