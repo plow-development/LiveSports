23ddb608053a1365ac3d2e9f1b39ae31bc7fcecd
RUAZD <96168542+RUAZD@users.noreply.github.com>
2022-07-15 09:33:14 +0300

    Обновлены описания моделей

--- app/models/models.py
+++ app/models/models.py
@@ -5,12 +5,12 @@ from pydantic import BaseModel, Field, EmailStr
 
 class UserOut(BaseModel):
     user_id: int = Field(None, description='ID пользователя')
-    username: str = Field(None, description='Ник пользователя')
+    username: str = Field(None, description='Псевдоним пользователя')
     email: EmailStr = Field(None, description='Почта пользователя')
-    avatar: str = Field(None, description='Аватарка пользователя')
+    avatar: str = Field(None, description='Ссылка на аватар пользователя')
     firstname: str = Field(None, description='Имя пользователя')
     lastname: str = Field(None, description='Фамилия пользователя')
-    birthday: date = Field(None, description='День рожденья пользователя')
+    birthday: date = Field(None, description='День Рожденья пользователя')
     money: int = Field(None, description='Баллы пользователя')
 
 
@@ -29,38 +29,38 @@ class TeamOut(BaseModel):
 
 
 class NewsOut(BaseModel):
-    news_id: int = Field(None)
-    title: str = Field(None)
-    content: str = Field(None)
-    preview: str = Field(None)
-    publictime: datetime = Field(None)
-    author_id: int = Field(None)
-    sport_id: int = Field(None)
+    news_id: int = Field(None, description='ID новости')
+    title: str = Field(None, description='Заголовок новости')
+    content: str = Field(None, description='Текст новости')
+    preview: str = Field(None, description='Ссылка на превью новости')
+    publictime: datetime = Field(None, description='Время публикации новости')
+    author_id: int = Field(None, description='ID пользователя - автора новости')
+    sport_id: int = Field(None, description='ID вида спорта, к которой относится новость')
 
 
 class BroadcastOut(BaseModel):
-    broadcast_id: int = Field(None)
-    title: str = Field(None)
-    description: str = Field(None)
-    preview: str = Field(None)
-    link: str = Field(None)
+    broadcast_id: int = Field(None, description='ID трансляции')
+    title: str = Field(None, description='Заголовок трансляции')
+    description: str = Field(None, description='Описание трансляции')
+    preview: str = Field(None, description='Ссылка на превью трансляции')
+    link: str = Field(None, description='Ссылка на трансляцию')
 
 
 class EventOut(BaseModel):
-    event_id: int = Field(None)
-    name: str = Field(None)
-    description: str = Field(None)
-    preview: str = Field(None)
-    starttime: datetime = Field(None)
-    latitude: float = Field(None)
-    longitude: float = Field(None)
-    sport_id: int = Field(None)
+    event_id: int = Field(None, description='ID мероприятия')
+    name: str = Field(None, description='Название мероприятия')
+    description: str = Field(None, description='Описание мероприятия')
+    preview: str = Field(None, description='Ссылка на превью мероприятия')
+    starttime: datetime = Field(None, description='Время начала мероприятия')
+    latitude: float = Field(None, description='Широта местоположения мероприятия')
+    longitude: float = Field(None, description='Долгота местоположения мероприятия')
+    sport_id: int = Field(None, description='ID вида спорта, к которой относится мероприятие')
 
 
 class UserComplex(BaseModel):
-    user: UserOut = Field(None, description='Участник')
-    teams: list[TeamOut] = Field(None, description='Команды, в которых состоит участник')
-    sports: list[SportOut] = Field(None, description='Предпочитаемые виды спорта')
+    user: UserOut = Field(None, description='Пользователь')
+    teams: list[TeamOut] = Field(None, description='Команды, в которых состоит пользователь')
+    sports: list[SportOut] = Field(None, description='Предпочитаемые виды спорта пользователя')
 
 
 class TeamComplex(BaseModel):
@@ -69,7 +69,8 @@ class TeamComplex(BaseModel):
     sport: SportOut = Field(None, description='Вид спорта команды')
     users: UserOut = Field(None, description='Участники команды')
 
+
 class NewsComplex(BaseModel):
-    news: NewsOut = Field(None)
-    author: UserOut = Field(None)
-    sport: SportOut = Field(None)
+    news: NewsOut = Field(None, description='Новость')
+    author: UserOut = Field(None, description='Автор новости')
+    sport: SportOut = Field(None, description='Вид спорта новости')
